7ef80bb18cbca64015009880831470a3950c6865
Fang Hui <hui.fang@nxp.com>
2021-11-23 21:30:22 +0800

    MA-19777 picture is 1944x1944 when take 2592x1944 picture on 8ulp with LCD panel
    getOrientation() already calls getRotationForOrientationValue() to return degrees.
    
    Change-Id: I10d1b336049d1305e1c302b7f400103d058dbfd8
    Signed-off-by: Fang Hui <hui.fang@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/17631
    Reviewed-by: Xuegang Liu <xuegang.liu@nxp.com>

--- src/com/android/camera/processing/imagebackend/TaskCompressImageToJpeg.java
+++ src/com/android/camera/processing/imagebackend/TaskCompressImageToJpeg.java
@@ -97,7 +97,7 @@ public class TaskCompressImageToJpeg extends TaskJpegEncode {
     public Map<Integer, Integer> exifGetMinimalTags(ExifInterface exif) {
         Map<Integer, Integer> map = new HashMap<>();
         map.put(ExifInterface.TAG_ORIENTATION,
-                ExifInterface.getRotationForOrientationValue((short) Exif.getOrientation(exif)));
+                Exif.getOrientation(exif));
         map.put(ExifInterface.TAG_PIXEL_X_DIMENSION, exif.getTagIntValue(
                 ExifInterface.TAG_PIXEL_X_DIMENSION));
         map.put(ExifInterface.TAG_PIXEL_Y_DIMENSION, exif.getTagIntValue(
