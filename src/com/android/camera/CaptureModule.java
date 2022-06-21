df633185c4f73b2c9d67c3b25898cd36a5bc65c8
Anle Pan <anle.pan@nxp.com>
2022-06-21 10:26:47 +0000

    MA-19645 Fix the crash caused by Resolution Switch issure
    On low performance device, with "Auto-rotate" off, and the display screen is portrait.
    open the Camera2 to launch preview activity,
    click camera seettings button and then return to the preview activity,
    Sometimes crash happened, the preview is in landscape and
    the seettings interface is in portrait when crashed.
    According to the crash log "Attempt to invoke virtual method
    'void com.android.camera.settings.SettingsManager.addListener
    (com.android.camera.settings.SettingsManager$OnSettingChangedListener)' on a null object reference",
    we can find that Null pointer variable caused the crash.
    
    This issue is analyzed as below: when returned back to the preview activity,
    CameraActivity has gone through 2 periods:
    onPause -> onDestroyTasks -> OnCreateTasks -> resume, Inside the resume() function,
    "openCameraAndStartPreview()" in CaptureModule.java will be called,
    and a new thread may be started in callback function "onCameraOpened".
    
    As mentioned aove, in this case there will be 2 lifecycle calls,
    At some extreme timing, the following sequence of operations will occur:
    onPause1 -> onDestroyTasks1 -> OnCreateTasks1 -> resume1(start thread1) ->
    onPause2 -> onDestroyTasks2 -> OnCreateTasks2 -> thread1 running, and crash.
    thread1 started at the first cycle, and the related variables have been destroyed later,
    but the thread1 is actually running in the second cycle.
    Therefore, when the thread1 function is executed further down,
    "mAppController.getSettingsManager()" is null, which caused the crash.
    
    To fix this issue, add a flag to ensure that the
    thread1 can not be executed after the first cycle is destroyed
    
    Change-Id: I5a970cbe85de6442c2b4f0960b6a189304c72477
    Signed-off-by: Anle Pan <anle.pan@nxp.com>
    Reviewed-on: http://androidsource.nxp.com/project/21373

--- src/com/android/camera/CaptureModule.java
+++ src/com/android/camera/CaptureModule.java
@@ -137,6 +137,9 @@ public class CaptureModule extends CameraModule implements
      */
     private final boolean mStickyGcamCamera;
 
+    /** run flag for new thread after open camera. */
+    private static boolean executor_flag = false;
+
     /** Controller giving us access to other services. */
     private final AppController mAppController;
     /** The applications settings manager. */
@@ -256,6 +259,7 @@ public class CaptureModule extends CameraModule implements
         @Override
         public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
             Log.d(TAG, "onSurfaceTextureDestroyed");
+            executor_flag = false;
             synchronized (mSurfaceTextureLock) {
                 mPreviewSurfaceTexture = null;
             }
@@ -772,6 +776,7 @@ public class CaptureModule extends CameraModule implements
 
     @Override
     public void destroy() {
+        executor_flag = false;
         mSoundPlayer.release();
         mMediaActionSound.release();
         mCameraHandler.getLooper().quitSafely();
@@ -1453,7 +1458,7 @@ public class CaptureModule extends CameraModule implements
                   public void onCameraOpened(@Nonnull final OneCamera camera) {
                       Log.d(TAG, "onCameraOpened: " + camera);
                       mCamera = camera;
-
+                      executor_flag = true;
                       // A race condition exists where the camera may be in the process
                       // of opening (blocked), but the activity gets destroyed. If the
                       // preview is initialized or callbacks are invoked on a destroyed
@@ -1484,8 +1489,11 @@ public class CaptureModule extends CameraModule implements
                       mMainThread.execute(new Runnable() {
                           @Override
                           public void run() {
-                              mAppController.getCameraAppUI().onChangeCamera();
-                              mAppController.getButtonManager().enableCameraButton();
+                              Log.d(TAG, "further settings after openCamera, run flag:" + executor_flag);
+                              if (executor_flag) {
+                                  mAppController.getCameraAppUI().onChangeCamera();
+                                  mAppController.getButtonManager().enableCameraButton();
+                              }
                           }
                       });
 
